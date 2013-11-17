b4d94358d3e50283f44736268f7e50fc7c2ee44e
andronik.ordian <andronik.ordian@gmail.com>
2013-11-18 00:00:33 +0400

    main loop

--- include/algorithm.hpp
+++ include/algorithm.hpp
@@ -1,71 +1,67 @@
 #include "graph.hpp"
 #include <boost/optional.hpp>
-
+double const EPS = 1e-9;
 
 struct Node
 {
-  Node(sz id, double distance)
-    : id_(id)
-    , distance_(distance)
-  {}
+	Node(sz id, double distance)
+		: id_(id)
+		, distance_(distance)
+	{}
 
-  sz id() const 
-  { 
-    return id_; 
-  }
+	sz id() const 
+	{ 
+		return id_; 
+	}
 
-  double const & distance() const 
-  { 
-    return distance_; 
-  }
+	double const & distance() const 
+	{ 
+		return distance_; 
+	}
 
-  double & distance()       
-  { 
-    return distance_; 
-  }
+	double & distance()       
+	{ 
+		return distance_; 
+	}
 
 private:
-  sz id_;
-  double distance_;
+	sz id_;
+	double distance_;
 };
 
 class ShortestPath
 {
  public:
  ShortestPath(sz from, sz to, Graph& g)
-    : from_(from)
-    , to_(to)
-    , graph_(g)
-    , prev_(g.num_v(), boost::optional<sz>())
-  {}
-   
-  struct Comparator 
-  {
-    bool operator() (Node const &u, Node const &v) 
-    {
-      return u.distance() < v.distance();
-    }
-  };
+		: from_(from)
+		, to_(to)
+		, graph_(g)
+		, prev_(g.num_v(), boost::optional<sz>())
+	{}
+	 
+	struct Comparator 
+	{
+		bool operator() (Node const &u, Node const &v) 
+		{
+			return u.distance() + EPS < v.distance();
+		}
+	};
 
-  
-  double dijkstra(); 
-  double aStar();
-     
-  void printPath(sz src, sz dst);
+	
+	double dijkstra(); 
+	double aStar();
+		 
+	void printPath(sz src, sz dst);
 
  private:
-  sz from_;
-  sz to_;
-  Graph & graph_;
-  vector<boost::optional<sz> > prev_;
-  
+	sz from_;
+	sz to_;
+	Graph & graph_;
+	vector<boost::optional<sz> > prev_;
+	
  private:
-  ShortestPath const& 
-    operator=(ShortestPath const&);
+	ShortestPath const& 
+		operator=(ShortestPath const&);
 
-  ShortestPath(ShortestPath const&);
+	ShortestPath(ShortestPath const&);
 };
-
-
-
-  
--- src/main.cpp
+++ src/main.cpp
@@ -6,68 +6,85 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+void option()
+{
+    cout << "OPTION is:"
+	 << endl << "0 - exit"
+	 << endl << "1 - Dijkstra (default)"
+	 << endl << "2 - A*"
+	 << endl << "3 - BiDijkstra"
+	 << endl << "4 - BiA*"
+	 << endl;
+}
+
 void usage()
 {
-    std::cerr
-            << "Usage: ./main USA-road-d.***.co USA-road-d.***.gr [OPTION]"
-            << endl << "OPTION is:"
-            << endl << "1 - Dijkstra (default)"
-            << endl << "2 - A*"
-            << endl << "3 - BiDijkstra"
-            << endl << "4 - BiA*"
-            << endl;
+    cout << "Usage: ./main "
+	 << "INPUT/USA-road-d.***.co "
+	 << "INPUT/USA-road-d.***.gr "
+	 << "[OPTION]";
+    option();
 }
 
 int runAlgorithm(Graph &g, int OPTION)
 {
-    sz from = 0;
-    sz to = 0;
+    while (OPTION)
+    {
+	sz from = 0;
+	sz to = 0;
 
-    cout << "Enter source vertex from 0 to "
-         << g.num_v() - 1
-         << ":"
-         << endl;
-    cin >> from;
-    if (from >= g.num_v())
-        return 1;
-
-    cout << "Enter destination vertex (same range)"
-         << ":"
-         << endl;
-
-    cin >> to;
-    if (to >= g.num_v())
-        return 1;
-
-  
-    ShortestPath d(from, to, g);
-    double weight = 0;
-    std::string msg = "Running ";
-
-    switch (OPTION) 
+	cout << "Enter source vertex from 0 to "
+	     << g.num_v() - 1
+	     << ":"
+	     << endl;
+	cin >> from;
+	if (from >= g.num_v())
+	    return 1;
+	
+	cout << "Enter destination vertex (same range)"
+	     << ":"
+	     << endl;
+	
+	cin >> to;
+	if (to >= g.num_v())
+	    return 1;
+	
+    
+      ShortestPath d(from, to, g);
+      double weight = 0;
+      std::string msg = "Running ";
+      
+      switch (OPTION) 
       {
       case 1:
-	weight = d.dijkstra();
-        msg += "Dijkstra...";
-	break;
+	  weight = d.dijkstra();
+          msg += "Dijkstra...";
+          break;
       case 2:
-	weight = d.aStar();
-	msg += "A*...";
-	break;
+	  weight = d.aStar();
+          msg += "A*...";
+          break;
       case 3:
-	break;
+	  cout << "Not implemented yet..." << endl;
+          return 1;
       case 4:
-	break;
+	  cout << "Not implemented yet..." << endl;
+          return 1;
       default:
-	return 1;
-      }
-    
-    cout << msg << endl;
+          return 1;
+        }
+      
+      cout << msg << endl;
 
-    d.printPath(from, to);
+      d.printPath(from, to);
 
-    cout << endl << "Weight: " << weight << endl;
-    return 0;
+      cout << endl << "Weight: " << weight << endl;
+      
+      cout << endl << "Choose OPTION:";
+      option();
+      cin >> OPTION;
+  }
+  return 0;
 }
 
 int main(int argc, char** argv)
